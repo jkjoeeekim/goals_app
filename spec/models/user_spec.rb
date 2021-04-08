9805ee7c0231058534290fe8b1eaa6308df54d3a
Joe <jkjoeeekim@live.com>
2021-04-07 19:19:34 -0700

    Add #generate session token rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,11 +18,12 @@ RSpec.describe User, type: :model do
             password: "good_password")
     end
 
+    # SHOULDA-MATCHERS SPECS
     it { should validate_uniqueness_of(:username) }
-
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
     it { should validate_length_of(:password).is_at_least(6) }
+
 
     it "creates a password_digest when a password is given" do
         expect(:password_digest).to_not be_nil
@@ -33,6 +34,7 @@ RSpec.describe User, type: :model do
         expect(user.session_token).to_not be_nil
     end
 
+
     describe '.find_by_credentials' do
         before { user.save! }
         it "returns user given good credentials" do
@@ -41,6 +43,18 @@ RSpec.describe User, type: :model do
 
         it "returns nil given bad credentials" do
             expect(User.find_by_credentials("joey", "wrong_password")).to eq(nil)
+        end
+    end
+
+    describe "#generate_session_token" do
+        it "creates a unique session_token every time" do
+            token1 = user.generate_session_token
+            token2 = user.generate_session_token
+            expect(token1).to_not eq(token2)
+        end
+
+        it "creates a session_token with a length of 32" do
+            expect(user.session_token.length).to eq(32)
         end
     end
 
