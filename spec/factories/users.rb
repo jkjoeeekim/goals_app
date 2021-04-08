fc26f73b9de478f1748c74b5bc23e7af2245311a
Joe <jkjoeeekim@live.com>
2021-04-07 18:08:22 -0700

    Add presence rspec for password_digest and session_token

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,20 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  username   :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string           not null
+#  session_token   :string           not null
 #
 FactoryBot.define do
     factory :user do
+        username { |n| Faker::Internet.username }
+        password { |n| Faker::Internet.password(min_length: 6) }
+    end
+
+    factory :username_check do
         username { |n| Faker::Internet.username }
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,12 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  username   :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string           not null
+#  session_token   :string           not null
 #
 require 'rails_helper'
 
@@ -19,4 +21,12 @@ RSpec.describe User, type: :model do
         it { should validate_uniqueness_of(:username) }
     end
 
+    describe 'presence' do
+        before(:each) do
+            create(:user)
+        end
+
+        it { should validate_presence_of(:password_digest) }
+        it { should validate_presence_of(:session_token) }
+    end
 end
